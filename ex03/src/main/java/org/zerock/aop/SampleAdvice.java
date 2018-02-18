bf30edb85802fb97149796dae3c291ab8f0d8bb8
SEOKRAE\SeokRae <kslbsh@gmail.com>
2018-02-18 18:59:12 +0900

    Commit Part4 Chapter 03 AOP Practice  - SampleAdvice

--- ex03/src/main/java/org/zerock/aop/SampleAdvice.java
+++ ex03/src/main/java/org/zerock/aop/SampleAdvice.java
@@ -3,6 +3,8 @@ package org.zerock.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -13,14 +15,29 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class SampleAdvice {
 
-  private static final Logger logger = LoggerFactory.getLogger(SampleAdvice.class);
+	private static final Logger logger = LoggerFactory.getLogger(SampleAdvice.class);
 
-  @Before("execution(* org.zerock.service.MessageService*.*(..))")
-  public void startLog(JoinPoint jp) {
+	@Before("execution(* org.zerock.service.MessageService*.*(..))")
+	public void startLog(JoinPoint jp) {
 
-    logger.info("----------------------------");
-    logger.info("----------------------------");
-    logger.info(Arrays.toString(jp.getArgs()));
-  }
+		logger.info("----------------------------");
+		logger.info("----------------------------");
+		logger.info(Arrays.toString(jp.getArgs()));
+	}
+
+	@Around("execution(* org.zerock.service.MessageService*.*(..))")
+	public Object timeLog(ProceedingJoinPoint pjp) throws Throwable {
+
+		long startTime = System.currentTimeMillis();
+		logger.info(Arrays.toString(pjp.getArgs()));
+
+		Object result = pjp.proceed();
+
+		long endTime = System.currentTimeMillis();
+		logger.info(pjp.getSignature().getName() + " : " + (endTime - startTime));
+		logger.info("=============================================");
+
+		return result;
+	}
 
 }
