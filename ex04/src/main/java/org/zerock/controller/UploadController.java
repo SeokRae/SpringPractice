df7b9c17cd79281552681ccdb60fa767a8930ad3
SEOKRAE\SeokRae <kslbsh@gmail.com>
2018-02-21 15:37:21 +0900

    Commit Part5 Chapter03 Controller reconstruction  - UploadController

--- ex04/src/main/java/org/zerock/controller/UploadController.java
+++ ex04/src/main/java/org/zerock/controller/UploadController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.util.UploadFileUtils;
 
 @Controller
 public class UploadController {
@@ -62,9 +63,10 @@ public class UploadController {
 	@RequestMapping(value = "/uploadAjax", method = RequestMethod.POST, produces = "text/plain; charset=UTF-8")
 	public ResponseEntity<String> uploadAjax(MultipartFile file) throws Exception {
 		logger.info("originalName : " + file.getOriginalFilename());
-		logger.info("size : " + file.getSize());
-		logger.info("contentType : " + file.getContentType());
+//		logger.info("size : " + file.getSize());
+//		logger.info("contentType : " + file.getContentType());
 		
-		return new ResponseEntity<>(file.getOriginalFilename(), HttpStatus.CREATED);
+		return new ResponseEntity<>(UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()), HttpStatus.CREATED);
 	}
+	
 }
