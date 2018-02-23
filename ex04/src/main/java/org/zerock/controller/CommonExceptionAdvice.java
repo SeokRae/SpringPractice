552f48ac7cf3f930544b06c0b98b15ea70d9d9d3
SEOKRAE\SeokRae <kslbsh@gmail.com>
2018-02-23 14:32:09 +0900

    Commit Part5 Chapter06 File upload Ajax  - SQL create tbl_attach  - BoardVO add attribute String[] files  - BoardDAO addAttach()  - boardMapper addAttach  - BoardDAO addAttach  - BoardService regist (Transaction) board + attach  - register.jsp (ajax)  - upload.js

---
+++ ex04/src/main/java/org/zerock/controller/CommonExceptionAdvice.java
@@ -0,0 +1,20 @@
+package org.zerock.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class CommonExceptionAdvice {
+
+//    private static final Logger logger = LoggerFactory.getLogger(CommonExceptionAdvice.class);
+    
+    @ExceptionHandler(Exception.class)
+    public ModelAndView errorModelAndView(Exception ex) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/error_common");
+        modelAndView.addObject("exception", ex);
+
+        return modelAndView;
+    }
+}
